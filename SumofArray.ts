982b8e248c782c79ee7a17bbfb7854e66f425602
yorliabdulai <yorliabdulai1@gmail.com>
2024-03-14 19:23:22 +0000

    Added sum of matrices

--- SumofArray.ts
+++ SumofArray.ts
@@ -1,6 +1,6 @@
 function SumArray(arr){
-    let sum = 0;
-    for(let i=0;i<arr.length;i++){
+    let sum =0;
+    for(let i=0; i< arr.length; i++){
         sum += arr[i];
     }
     return sum;
---
+++ sumMatrices.js
@@ -0,0 +1,11 @@
+function SumMatrices(Matrix1, Matrix2){
+    const result = [];
+    for(let i =0;i<Matrix1.length; i++){
+        const sum = [];
+        for(let j=0;j<Matrix1[i].length; j++){ 
+           sum[j] = Matrix1[i][j] + Matrix2[i][j];
+        } 
+        result += sum; 
+    }
+    return result;
+}
